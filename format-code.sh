2674e9ee17d4ba5ba0e93d263739f3ff49bc9a3c
Richard Attermeyer <richard.attermeyer@opitz-consulting.com>
2020-07-10 22:00:26 +0200

    made small changes to work better with pre-commit

--- format-code.sh
+++ format-code.sh
@@ -4,11 +4,9 @@ mkdir -p .cache
 cd .cache
 if [ ! -f "google-java-format-${VERSION}-all-deps.jar" ]
 then
-    curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-${VERSION}/google-java-format-${VERSION}-all-deps.jar"
+    curl -sLJO "https://github.com/google/google-java-format/releases/download/google-java-format-${VERSION}/google-java-format-${VERSION}-all-deps.jar"
     chmod 755 google-java-format-${VERSION}-all-deps.jar
 fi
 cd ..
 
-changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
-echo $changed_java_files
-java -jar ".cache/google-java-format-${VERSION}-all-deps.jar" --replace $changed_java_files
+java -jar ".cache/google-java-format-${VERSION}-all-deps.jar" --replace $@
